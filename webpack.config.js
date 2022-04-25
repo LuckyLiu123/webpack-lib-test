b6f09fd3624c1cf2fb31bcd4429210f9feecdb54
liukai <15818524733@139.com>
2022-04-25 22:18:20 +0800

    webpack打包公共库

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const terserPlugin = require('terser-webpack-plugin')
+
+module.exports = {
+    // entry: './src/index.js',
+    entry: {
+        'add-number': './src/index.js',
+        'add-number.min': './src/index.js',
+    },
+    output: {
+        path: path.resolve(__dirname, './umd'),
+        filename: '[name].js',
+        library: 'kkb',   //指定库的名称
+        libraryTarget: 'umd'  //指定打包的规范  var this window global umd
+    },
+    //打包压缩文件和未压缩文件的时候mode不起作用，需要使用terser-webpack-plugin
+    mode: "none",  //development production
+    optimization: {
+        minimize: true,  //是否开启压缩
+        // minimizer 类似于 plugins
+        minimizer: [
+            new terserPlugin({
+                test: /\.min\.js$/,  //以.min.js结尾的文件开启压缩
+            })
+        ]
+    }
+}
